70bd0b61262c9f47ef8e4b5237f40e6f9589e6a8
Miical <879754743@qq.com>
2023-12-11 11:08:41 +0800

    change register to mlvp

--- register/tb/test_register.py
+++ register/tb/test_register.py
@@ -1,25 +1,32 @@
 '''
 A cocotb-pytest test (this file) has two parts:
 
-1. Testbench 
+1. Testbench
     - Any python function decorated with @cocotb.test()
     - Drives signals into pins of the design, reads the output/intermediate pins and compares with expected results
-    - Uses async-await: 
+    - Uses async-await:
         - Declared as def async
         - when "await Event()", simulator advances in simulation time until the Event() happens
     - You can have multiple such testbenches too. Pytest would find and run them all
-2. PyTest 
-    - The setup that connects the simulator of your choice, 
-    - Feeds the design files, 
-    - Finds your testbenches (1), 
+2. PyTest
+    - The setup that connects the simulator of your choice,
+    - Feeds the design files,
+    - Finds your testbenches (1),
     - Parametrizes them to generate multiple versions of the designs & tests
     - Runs all such tests and prints a report of pass & fails
 '''
 
 
-import cocotb
-from cocotb.clock import Clock
-from cocotb.triggers import Timer, RisingEdge, FallingEdge, ClockCycles
+
+import sys
+sys.path.append('/home/miical/Projects/cocotb-example/register/hdl/')
+sys.path.append("/home/miical/Projects/PyMLVP/src/")
+
+import mlvp
+from mlvp import *
+from mlvp.triggers import *
+from ovip import UTregister
+
 
 import random
 # import numpy as np
@@ -27,26 +34,26 @@ import random
 '''
 1. Testbench
 '''
-@cocotb.test()
+
 async def register_tb(dut):
 
     ''' Clock Generation '''
-    clock = Clock(dut.clk, 10, units="ns") # create a clock for dut.clk pin with 10 ns period
-    cocotb.start_soon(clock.start()) # start clock in a seperate thread
+    mlvp.create_task(mlvp.start_clock(dut, dut.clk))
+    print("Clock started")
 
     ''' Assign random values to input, wait for a clock and verify output '''
     for i in range(100): # 100 experiments
-        
+
         exact = random.randint(0, 255) # generate randomized input
         dut.d.value = exact # drive pins
-        
-        await FallingEdge(dut.clk) # wait for falling edge
-        
-        computed = dut.q.value.integer # Read pins as unsigned integer.
+
+        await ClockCycles(dut, 2)
+
+        computed = dut.q.value # Read pins as unsigned integer.
         # computed = dut.q.value.signed_integer # Read pins as signed integer.
-        
+
         assert exact == computed, f"Failed on the {i}th cycle. Got {computed}, expected {exact}" # If any assertion fails, the test fails, and the string would be printed in console
-        print(f"Driven value: {exact} \t received value: {computed}") 
+        print(f"Driven value: {exact} \t received value: {computed}")
 
 
 
@@ -54,26 +61,7 @@ async def register_tb(dut):
 2. Pytest Setup
 '''
 
-from cocotb_test.simulator import run
-import pytest
-import glob
 
-@pytest.mark.parametrize(
-    # Two sets of parameters to test across
-    "parameters", [
-        {"WIDTH_IN": "8", "WIDTH_OUT": "16"},
-        {"WIDTH_IN": "16"}
-        ])
-def test_register(parameters):
-
-    run(
-        verilog_sources=glob.glob('register/hdl/*'),
-        toplevel="register",    # top level HDL
-        
-        module="test_register", # name of the file that contains @cocotb.test() -- this file
-        simulator="icarus",
-
-        parameters=parameters,
-        extra_env=parameters,
-        sim_build="register/sim_build/" + ",".join((f"{key}={value}" for key, value in parameters.items())),
-    )
+def test_register():
+    dut = UTregister(0, None)
+    mlvp.run(register_tb(dut))
